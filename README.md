01f0f446e76bba8927ed0d9f4b2ab6c09ccd3d09
HegmannT <77809977+HegmannT@users.noreply.github.com>
2021-04-07 23:16:49 -0600

    Added files via upload
    Added files via upload from computer

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
-# ENGO551FinalProject
+ENGO 551
+Winter 2021
+
+Final Group Project
+
+By:
+Alekhya Bhamidipati
+Madison Marriott
+Tanya Hegmann
+-----------------------------------------------------------------
+To run this code on the computer, navigate to the folder containing the files and open the cmd window
+
+Please run 
+py -m pip install -r requirements.txt //Folder containing requirements
+py -m pip install requests
+
+set FLASK_APP=application.py
+set FLASK_DEBUG=1
+set DATABASE_URL=postgres://kevshpuonrwgfr:c8535cc284c9df961ecf0aa1657793e8de1632987c584b02216bbe401f4cb6f3@ec2-54-205-183-19.compute-1.amazonaws.com:5432/d67i21u3n79vhb
+And then to run the code: 
+py -m flask run
+
+-----------------------------------------------------------------
+There are 8 Webpages for this website:
+
+/ 
+- The Home Page
+- Greets the User Upon Coming to the Website
+- Has a google maps API of Calgary for bonus
+- If a user session is detected, load template homesignedin.html 
+	- Prompts a signed in user to search a community or log out
+		- Logged out users are taken to /logout
+		- Users looking to search a community are taken to /calgarycommunityhousingmap
+- If no user session is detected, load template home.html
+	- Prompts a user to sign up or sign in  
+		- If a user signs in they are taken to /signin
+		- If a user signs up they are take to /signup
+
+
+/signin
+- Allows users to sign in to a pre-existing account to access the website
+- User is loaded in signin.html
+- Users can sign in with a username and password
+	- Upon signing in they are taken to /signincheck
+- Option for user to return to home page
+	- User is taken to /
+- Option for user to go to Sign-Up instead
+	- User is taken to /signup
+
+
+/signincheck
+- The credentials entered by users in /signin are checked against the database for username/password match
+- If credentials are correct, user is loaded signinsuccess.html
+	- The user is notified that they have succesfully signed in with their username
+	- The user had the option to
+		- Return to home going to / 
+		- Log out of their account going to /logout
+		- Search Calgary communities going to /calgarycommunityhousingmap
+- If credentials fail and sign in fails user is loaded signinfail.html
+	- The user is told their sign in failed with that username
+	- The user has the option to
+		- Return to home going to / 
+		- Signing in again going to /signin
+		- Signing up a new account going to /signup
+
+
+/signup
+- Allows users to sign up to a pre-existing account to access the website
+- User is loaded in signup.html
+- Users can sign in with a username and password
+	- Upon signing up they are taken to /signupcheck
+- Option for user to return to home page
+	- User is taken to /
+- Option for user to go to Sign-In instead
+	- User is taken to /signin
+
+
+/signupcheck
+- The credentials entered by users in /signup are checked against the database for if the username is taken
+- If credentials are correct, user is loaded signupsuccess.html
+	- The username is stored in the database
+	- The user is notified that they have succesfully signed up with their username
+	- The user had the option to
+		- Return to home going to / 
+		- Log out of their account going to /logout
+		- Search Calgary communities going to /calgarycommunityhousingmap
+- If credentials fail and sign in fails user is loaded signupfail.html
+	- The user is told their sign up failed with that username
+	- The user has the option to
+		- Return to home going to / 
+		- Signing in to a pre-existing account going to /signin
+		- Try Signing up a new account again going to /signup
+
+
+/logout
+- The user logs out and the session ends
+- The user has the option to 
+	- Return to home going to / 
+	- Signing in to a pre-existing account going to /signin
+	- Sign up with a new account going to /signup
+
+
+/calgarycommunityhousingmap
+- The Main website page for the user
+- Contains the main interactive map application (more details below)
+- The user has the option to 
+	- Return to home going to / 
+	- Refresh the page/map and return to /calgarycommunityhousingmap
+	- Log out go to /logout
+	- Return to top of page (if at bottom of page)
+
+
+
+/APIendpointPAGE
+- TBA
+
+
+-----------------------------------------------------------------
+Information on the Map:
+
+TBA
+
+
+
+
+
+
+
---
+++ application.py
@@ -0,0 +1,167 @@
+#-------------------------------------------------------------------------------
+# Name:         ENGO 551, Winter 2021, Final Project
+# Student:      Alekhya Bhamidipati
+#               Madison Marriott
+#               Tanya Hegmann
+# Created:      07-04-2021
+#-------------------------------------------------------------------------------
+#---------------SETUP---------------
+
+import os
+import csv #added on for csv read in
+import json #built in json package to work with data
+import requests
+
+from flask import Flask, session, render_template, request, jsonify #Note added in request & render_template as they do in the lectures
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database from set URL
+# DATABASE_URL = postgres://kevshpuonrwgfr:c8535cc284c9df961ecf0aa1657793e8de1632987c584b02216bbe401f4cb6f3@ec2-54-205-183-19.compute-1.amazonaws.com:5432/d67i21u3n79vhb
+# Database Table referenced for login is = userlogin2
+engine = create_engine(os.getenv("DATABASE_URL")) #Already preset as enviro variable
+db = scoped_session(sessionmaker(bind=engine)) #main object to run SQL commands
+
+
+#---------------------------------------------------------------------
+# --------------- HOMEPAGE ---------------
+
+@app.route("/") #Home Route
+def index():
+
+
+    if 'user' in session: #check if currently signed in, ie, is a session in play
+        username = session['user']
+        return render_template("homesignedin.html", username = username) #Load HomePage where Session is Already Signed In
+
+    return render_template("home.html") #Else Load Triaditional Home Page prompting to sign in
+
+
+
+#---------------------------------------------------------------------
+# --------------- lOG IN/SIGN IN PAGE ---------------
+
+@app.route("/signin") #Sign in Route
+def signin():
+
+    return render_template("signin.html") #Load Sign In Page
+
+#---------------------------------------------------------------------
+# --------------- lOG IN/SIGN IN VERIFICATION PAGE ---------------
+
+@app.route("/signincheck", methods=["POST"]) #Page to check if Log In Sucseeded or Failed
+def signincheck():
+    #check against database here
+
+    #Assign temp variables to use as comparisons
+    username = request.form.get("username_old")
+    password = request.form.get("password_old")
+
+    #Check against database
+
+    checkuser = db.execute("SELECT * FROM userlogin2 WHERE (username = '{username}') AND (password = '{password}')".format(username = username, password = password)).fetchone()
+
+    #if return false
+    if checkuser is None:
+        #ERROR SIGN IN TEMPLATE RUN
+        return render_template("signinfail.html", username = username, password = password) #Load page for sign in Fail
+
+    #else if return true...
+
+    #start session
+    session["user"] = username
+
+    #run correct sign in
+    return render_template("signinsuccess.html", username = username, password = password) #Load page for sign in Sucsess
+
+#---------------------------------------------------------------------
+# --------------- SIGN UP PAGE ---------------
+
+@app.route("/signup") #Load Page to Sign Up At
+def signup():
+
+    return render_template("signup.html")
+
+#---------------------------------------------------------------------
+# --------------- SIGN UP VERIFICATION PAGE ---------------
+
+@app.route("/signupcheck", methods=["POST"])
+def signupcheck():
+    #check against database here
+
+    #Assign temp variables to use as comparisons
+    username = request.form.get("username_new")
+    password = request.form.get("password_new")
+
+    #Check against database if Username already exists
+    #Only need to check if username exists
+    checkuser = db.execute("SELECT * FROM userlogin2 WHERE (username = '{username}')".format(username = username)).fetchone()
+
+    #If username exists
+        #error try again page
+
+    #If Username does not already exist
+    if checkuser is None:
+        #Add Data to List
+        db.execute("INSERT INTO userlogin2 (username, password) VALUES (:username, :password)",{"username":username, "password":password})
+        db.commit()#commit changes
+
+        #start session
+        session["user"] = username
+
+        return render_template("signupsuccess.html", username = username, password = password)
+
+    #else if return true (aka username already exists)
+    #run failed sign in
+    return render_template("signupfail.html", username = username, password = password)
+
+#---------------------------------------------------------------------
+# --------------- LOGOUT PAGE ---------------
+
+@app.route("/logout", methods=["POST"]) #Page where you can log out and end the session
+def logout():
+
+    username = session["user"] #retrieve username
+
+    session.pop("user", None) #remove session, log out
+    session.pop("book", None) #remove session data
+
+    return render_template("userlogout.html", username = username) #load book search page
+
+
+#---------------------------------------------------------------------
+# --------------- MAIN MAP PAGE ---------------
+
+@app.route("/calgarycommunityhousingmap", methods=["POST"])
+def calgarycommunityhousingmap():
+
+    #retrieve session info
+    username = session["user"]
+
+    return render_template("calgarycommunityhousingmap.html", username = username) #load book search page
+
+
+#---------------------------------------------------------------------
+# --------------- API ENDPOINT CODE ---------------
+
+# Put API Endpoint page like books lab here
+
+
+
+
+
+
+
+
